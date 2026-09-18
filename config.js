7cf0787b1c0a7410af286aedd8fdcacb75f7e4c2
gamarratv02-create <gamarratv02@gmail.com>
2026-09-18 10:35:54 -0500

    config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+// Configuración pública de Vallenateando Radio
+const SUPABASE_URL = "https://qfyrjqrcubljvdjvybss.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_6ytGRv66Ybi6egDTVIiyKA_lGOtz-Nj";
+const STREAM_URL = "https://djp.sytes.net/hls/vallenateandoradio/live.m3u8";
